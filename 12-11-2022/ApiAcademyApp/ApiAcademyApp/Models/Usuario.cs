5958a2c37cc9b24072dbe7b0e2ce51a93828a86a
Sebastián Murillo <sebasmu223@gmail.com>
2022-10-11 22:54:04 -0600

    Se creó el proyecto API web, así como una sencilla capa de de datos para la inserción y modificación de datos.

---
+++ 12-11-2022/ApiAcademyApp/ApiAcademyApp/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiAcademyApp.Models
+{
+    public class Usuario
+    {
+        //Esta clase basicamente es la encargada de tener exactamente los mismos datos que la base de datos para
+        //así tener un manejo más fácil al hacer la conexion y tratar los datos de manera más sencilla.
+        public int Id { get; set; } 
+        public string Nombre { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+        public int Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Password { get; set; }
+    }
+}
